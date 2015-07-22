80c4692c77cbdcd7629288544521eabebd1ac882
kushasharma <kushasharma@paypal.con>
2015-07-22 16:19:33 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Odometer
+
+Takes an initial reading as a number and convert to an integer array which is used to represent the digits in an odometer.
+
+The following functions are supported:
+
+isValid() : Checks if the reading is valid
+increment() : finds the next valid reading
+decrement(): finds the previous valid reading
+
+The following helper functions are internally used:
+
+maxAtIndex() : finds the maximum possible digit at a given position in the odometer.
+minAtIndex() : finds the minimum possible digit at a given position in the odometer.
--- src/Odometer.java
+++ src/Odometer.java
@@ -6,7 +6,31 @@ public class Odometer {
     private int[] currentReading;
     private int size;
 
+
     public Odometer(int initialReading) {
+    }
+
+    public int maxAtIndex(int index, int length)
+    {
+        return 9 - length + (index + 1);
+    }
+
+    public int minAtIndex(int index, int length)
+    {
+        return index+1;
+    }
+
+    public boolean isValid(int[] number)
+    {
+        for (int i = number.length-1; i >= 0; i--)
+        {
+            if ((number[i] < minAtIndex(i,number.length)) || (number[i] > maxAtIndex(i,number.length)))
+                return false;
+
+            if ((i > 0) & (number[i] < number[i-1]))
+                return false;
+        }
+        return true;
     }
 
     public int[] increment(int[] oldReading) {
