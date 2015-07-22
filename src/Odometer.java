c9e0b43efed4f1957d603d79148c7eab876780db
kushasharma <kushasharma@paypal.con>
2015-07-22 15:21:54 +0530

    Add boilerplate code

---
+++ src/Odometer.java
@@ -0,0 +1,20 @@
+/**
+ * Created by test on 7/22/2015.
+ */
+public class Odometer {
+
+    private int[] currentReading;
+    private int size;
+
+    public Odometer(int initialReading) {
+    }
+
+    public int[] increment(int[] oldReading) {
+        for(int pos = size - 1; pos >= 0; pos --)  {
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
